da9d41cae5b675b30cd44dd822c2d183139b9eb8
bebe0601 <jrhigasinihonn.0601@gmail.com>
2016-11-17 12:24:04 +0900

    Refactor

--- src/main/java/com/kamesuta/mc/signpic/plugin/gui/GuiManager.java
+++ src/main/java/com/kamesuta/mc/signpic/plugin/gui/GuiManager.java
@@ -27,7 +27,6 @@ import com.kamesuta.mc.signpic.Reference;
 import com.kamesuta.mc.signpic.entry.EntryId;
 import com.kamesuta.mc.signpic.gui.SignPicLabel;
 import com.kamesuta.mc.signpic.plugin.SignData;
-import com.kamesuta.mc.signpic.plugin.gui.GuiManager.GuiGallery.GalleryPanel.GalleryLabel;
 import com.kamesuta.mc.signpic.plugin.packet.PacketHandler;
 import com.kamesuta.mc.signpic.plugin.packet.PacketHandler.SignPicturePacket;
 import com.kamesuta.mc.signpic.render.RenderHelper;
@@ -35,8 +34,6 @@ import com.kamesuta.mc.signpic.render.RenderHelper;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.util.ResourceLocation;
 
 public class GuiManager extends WFrame implements IGuiControllable {
 	public static int row = 4;
@@ -368,142 +365,6 @@ public class GuiManager extends WFrame implements IGuiControllable {
 				this.lastSelect = next;
 			}
 			return super.keyTyped(ev, pgp, p, c, keycode);
-		}
-
-		public class MouseOverPanel extends WPanel {
-			protected final String localizationOwner;
-			protected String uri;
-			protected String world;
-			protected String owner;
-			protected GalleryLabel label;
-
-			public MouseOverPanel(final R position) {
-				super(position);
-				this.localizationOwner = I18n.format("signpic.gui.manager.owner");
-			}
-
-			public void setLabel(final GalleryLabel label) {
-				if (this.label==label)
-					return;
-
-				this.label = label;
-				if (this.label!=null) {
-					final SignData data = this.label.getData();
-					this.owner = this.localizationOwner+":"+data.owner_name;
-					this.world = "World:"+data.world;
-					final String uri = new EntryId(data.sign).getContentId().getURI();
-					String substringURI = uri;
-					while (getStringWidth(substringURI)>120)
-						substringURI = substringURI.substring(0, substringURI.length()-1);
-					if (uri.length()==substringURI.length())
-						this.uri = substringURI;
-					else
-						this.uri = substringURI+"...";
-				}
-			}
-
-			public void openMenu(final Point p) {
-				if (this.label!=null&&!isOpenMenu())
-					this.openMenuPoint = p;
-			}
-
-			public boolean isOpenMenu() {
-				return getContainer().size()!=0;
-			}
-
-			@Override
-			protected void initWidget() {
-				invokeLater(new Runnable() {
-					@Override
-					public void run() {
-						if (getContainer().size()>=1)
-							getContainer().clear();
-					}
-				});
-				super.initWidget();
-			}
-
-			Point openMenuPoint;
-
-			@Override
-			public void update(final WEvent ev, final Area pgp, final Point p) {
-				if (this.openMenuPoint!=null) {
-					final Area a = getGuiPosition(pgp);
-					final float left = this.openMenuPoint.x()<115 ? 0 : this.openMenuPoint.x()-115;
-					final float top = this.openMenuPoint.y()>a.y2()-80 ? this.openMenuPoint.y()-80 : this.openMenuPoint.y();
-					final R position = new R(Coord.left(left), Coord.top(top), Coord.height(79), Coord.width(115));
-					add(new GuiClickMenu(position, this, GuiManager.this) {
-						@Override
-						protected void initWidget() {
-							add(new ClickMenuPanel(I18n.format("signpic.gui.manager.open")) {
-								{
-									setEmphasis(true);
-									setIcon(new ResourceLocation("signpic", "textures/logo.png"));
-								}
-
-								@Override
-								public boolean onEnter(final WEvent ev, final Area pgp, final Point p) {
-									return true;
-								}
-							});
-							add(new ClickMenuPanel(I18n.format("signpic.gui.manager.openbrowzer")) {
-								@Override
-								public boolean onEnter(final WEvent ev, final Area pgp, final Point p) {
-									return true;
-								}
-							});
-							add(new ClickMenuPanel("sushi") {
-								@Override
-								public boolean onEnter(final WEvent ev, final Area pgp, final Point p) {
-									return true;
-								}
-							});
-							add(new ClickMenuPanel("sushi") {
-								@Override
-								public boolean onEnter(final WEvent ev, final Area pgp, final Point p) {
-									return true;
-								}
-							});
-							add(new ClickMenuPanel("sushi") {
-								@Override
-								public boolean onEnter(final WEvent ev, final Area pgp, final Point p) {
-									return true;
-								}
-							});
-							super.initWidget();
-						}
-					});
-					this.openMenuPoint = null;
-				}
-				super.update(ev, pgp, p);
-			}
-
-			@Override
-			public void draw(final WEvent ev, final Area pgp, final Point p, final float frame, final float popacity) {
-				final Area a = getGuiPosition(pgp);
-				if (this.label!=null&&!isOpenMenu()) {
-					final float x1 = p.x()<a.x2()-140 ? p.x()+8 : p.x()-140;
-					final float x2 = p.x()+140<a.x2() ? p.x()+140 : p.x()-8;
-					final float y1 = p.y()>25 ? p.y()-25 : p.y();
-					final float y2 = p.y()>25 ? p.y() : p.y()+25;
-					final Area overlay = new Area(x1, y1, x2, y2);
-					glColor4f(0, 0, 0, 1);
-					RenderHelper.startShape();
-					drawRect(overlay);
-					glLineWidth(4f);
-					glColor4f(.1f, 0, .2f, 1);
-					draw(overlay, GL_LINE_LOOP);
-					glPushMatrix();
-					glTranslated(overlay.minX()+overlay.w()/2, overlay.minY()+overlay.h()/2, 0);
-					RenderHelper.startTexture();
-					drawString(this.owner, overlay.minX()-overlay.maxX()+70, overlay.minY()-overlay.maxY()+15, 0xffffff);
-					drawStringR(this.world, overlay.minX()-overlay.maxX()+195, overlay.minY()-overlay.maxY()+15, 0xffffff);
-					drawString(this.uri, overlay.minX()-overlay.maxX()+70, overlay.minY()-overlay.maxY()+26, 0xffffff);
-					glPopMatrix();
-				}
-
-				super.draw(ev, pgp, p, frame, popacity);
-			}
 		}
 
 		public class GalleryPanel extends WPanel {
